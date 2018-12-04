f76e83da7c95f9c1453ca5fb279eede739124db1
Tomasz <35335044+czomo@users.noreply.github.com>
2018-12-04 10:21:26 +0100

    Update JavaDemo.java

--- JavaDemo.java
+++ JavaDemo.java
@@ -1,9 +1,6 @@
-import java.io.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.*;
-import java.util.Calendar;
+import java.util.Scanner;
+
 public class JavaDemo{
  
 public static void main(String[] args)
