8d4b9e41e52a002a35b10ef98be88255df1d685e
Tomasz <35335044+czomo@users.noreply.github.com>
2018-12-07 00:20:26 +0100

    Update JavaDemo.java

--- JavaDemo.java
+++ JavaDemo.java
@@ -1,58 +1,57 @@
 import java.sql.*;
-import java.util.Scanner;
 
-public class JavaDemo{
- 
-public static void main(String[] args)
-    {
+public class JavaDemo {
+   public static void main(String[] args) {
         Connection conn = null;
-        Statement statement;
-         try {
-             Class.forName("com.mysql.jdbc.Driver");
-             conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?" +
-                                        "user=tomasz&password=dul");
-        String st = "CREATE TABLE IF NOT EXISTS table (tytul VARCHAR(30), tresc VARCHAR(100), autor VARCHAR(30));";
-        statement = conn.createStatement();
-        statement.executeUpdate(st);
-         } catch (SQLException e) {
-            System.out.println("Error connection");
-        }
-        Scanner in = new Scanner(System.in);
-        while(true){
-        System.out.println("Wpisz dod aby dodać do bazy lub dis aby wyświetlić");
-        String com = in.nextLine();
-            switch(com){
-            case "dis":
-                try{
-                    statement = conn.createStatement();
-                    String query1 = "SELECT * FROM table;";
-                    ResultSet result = statement.executeQuery(query1);
-                    while (result.next()) {
-                        System.out.println(
-                            "Tytul:" + result.getString("tytul") + 
-                            "Tresc:" + result.getString("tresc") + 
-                            "Autor:" + result.getString("autor")
-                        );
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://10.0.10.3:3306/mydb?autoReconnect=true&useSSL=false", "tomasz", "dul");
+
+            stmt = conn.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS myTable" +
+                    " (tytul VARCHAR(30)," +
+                    " tresc VARCHAR(30)," +
+                    " autor VARCHAR(30))";
+            stmt.executeUpdate(sql);
+        	System.out.println("podlaczono do bazy");
+            if(args.length > 0) {
+                if(args[0].equals("dis")) {
+                    PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM myTable");
+                    ResultSet res = preparedStatement.executeQuery();
+                    while(res.next()) {
+                        System.out.println("" +
+                                "tytul " + res.getString("tytul") +
+                                "   tresc " + res.getString("tresc") +
+                                "   autor " + res.getString("autor"));
                     }
-               }catch (SQLException ex){
-                    System.out.println("Error insert");
-               }                break;
-            case "dod":
-               try{
-                    statement = conn.createStatement();
-                    System.out.print("Tytul: ");
-                    String tytul = in.nextLine();
-                    System.out.print("Tresc: ");
-                    String tresc = in.nextLine();
-                    System.out.print("Autor: ");
-                    String autor = in.nextLine();
-                    String query2 = "INSERT INTO table VALUES('"+tytul+"', '"+tresc+"', '"+autor+"');";
-                    statement.executeUpdate(query2);
-               }catch (SQLException ex){
-                    System.out.println("Error insert");
-               }
-               break;
-    }
+                }
+                else if(args[0].equals("dod")) {
+                    PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO myTable VALUES (?,?,?)");
+                    preparedStatement.setString(1, args[1]);
+                    preparedStatement.setString(2, args[2]);
+                    preparedStatement.setString(3, args[3]);
+                    preparedStatement.executeUpdate();
+                }
+                else
+                    System.out.println("brak danych ");
+            }
         }
-}
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                if(stmt != null)
+                    conn.close();
+            } catch (SQLException e) { }
+            try{
+                if(conn != null){
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
